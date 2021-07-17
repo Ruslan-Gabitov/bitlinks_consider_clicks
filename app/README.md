4e79398423c5e731396366c10390d2e51483b494
Ruslan-Gabitov <rgabitov355@gmail.com>
2021-07-18 01:39:58 +0500

    Initial commit

---
+++ app/README.md
@@ -0,0 +1,43 @@
+# Bitly url shorterer
+
+Enter a long link to get a short
+
+```
+Input:
+$ python3 main.py https://dvmn.org/modules/web-api/lesson/migration-from-website/
+Output:
+https://bit.ly/2VNYLdL
+```
+Enter a short link to get the number of clicks on it
+
+```
+Input:
+$ python3 main.py https://bit.ly/2VNYLdL
+Output:
+Number of clicks on the link: 10
+```
+
+### How to install
+
+To authorize, you must obtain a TOKEN, you can obtain it from this site [dev.bitly.com](https://dev.bitly.com/api-reference). Token must be saved to .env file
+
+``` python
+import os
+from dotenv import load_dotenv
+
+if __name__ == '__main__':
+    project_folder = os.path.expanduser('~/Path to .env/')
+    load_dotenv(os.path.join(project_folder, '.env'))
+    token = {'Authorization': os.getenv('BITLY_API_KEY')}
+
+```
+
+Python3 should be already installed. 
+Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
---
+++ app/main.py
@@ -0,0 +1,49 @@
+import os
+import requests
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+import argparse
+
+def shorten_link(link, headers):
+    url = 'https://api-ssl.bitly.com/v4/shorten'
+    payloads = {'long_url': link}
+    response = requests.post(url, json=payloads, headers=headers)
+    response.raise_for_status()
+    abbreviated_link = response.json()
+    return abbreviated_link['link']
+
+
+def count_clicks(link, headers):
+    payloads = {'units': -1}
+    split_link = urlparse(link)
+    complete_url = f'https://api-ssl.bitly.com/v4/bitlinks/{"".join(split_link._replace(scheme=""))}/clicks/summary'
+    response = requests.get(complete_url, params=payloads, headers=headers)
+    response.raise_for_status()
+    clicks_count = response.json()
+    return clicks_count['total_clicks']
+
+
+def has_abbreviated_link(link, headers):
+    split_link = urlparse(link)
+    complete_url = f'https://api-ssl.bitly.com/v4/bitlinks/{"".join(split_link._replace(scheme=""))}'
+    response = requests.get(complete_url, headers=headers)
+    return response.ok
+
+
+
+if __name__ == '__main__':
+    project_folder = os.path.expanduser('~/Desktop/Projects/bitlinks_consider_clicks/app')
+    load_dotenv(os.path.join(project_folder, '.env'))
+    token = {'Authorization': os.getenv('BITLY_API_KEY')}
+    parser = argparse.ArgumentParser(description='Сокращает ссылки и показывает количество кликов по сокращенной ссылке')
+    parser.add_argument('link', help='Ваша ссылка')
+    args = parser.parse_args()
+
+    # link = input('Введите ссылку: ')
+    try:
+        if has_abbreviated_link(args.link, token):
+            print(f'Количество кликов по ссылке: {count_clicks(args.link, token)}')
+        else:
+            print(shorten_link(args.link, token))
+    except requests.exceptions.HTTPError:
+        print('Вы ввели некорректную ссылку!')
